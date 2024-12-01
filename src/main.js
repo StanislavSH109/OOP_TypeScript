1c70402356011331b371746f102e1d50f0429a25
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-12-01 19:12:54 +0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+var Car = /** @class */ (function () {
+    function Car(model, color) {
+        this.model = model;
+        this.color = color;
+    }
+    return Car;
+}());
+console.log(Object);
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+class Car {
+    public model: string;
+    color: string;
+    constructor(model: string, color: string) {
+        this.model = model;
+        this.color = color;
+    }
+}
+
+console.log(Object);
