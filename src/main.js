57ff2ca3c56cb7bc5ecd8a3f8d7c3e55378957fa
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-12-01 19:28:30 +0300

    added public method changeColor for change private property Color

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ var Car = /** @class */ (function () {
         this.model = model;
         this.color = color;
     }
+    Car.prototype.changeColor = function (newColor) {
+        this.color = newColor;
+    };
     return Car;
 }());
 var myCar = new Car('BMW', 'red');
 console.log(myCar.model);
-console.log(myCar.color);
+myCar.changeColor('White');
+console.log(myCar);
+//Оба свойства и public и без модификатора доступа доступны без ограничений.
+// Если мы применим private к свойству color, то TS сразу подсветит ошибку о том, что вне класса свойство не может применяться.
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,16 @@ class Car {
         this.model = model;
         this.color = color;
     }
+
+    changeColor(newColor: string) {
+        this.color = newColor;
+    }
 }
 
 const myCar = new Car('BMW', 'red')
 console.log(myCar.model);
-console.log(myCar.color);
-
+myCar.changeColor('White');
+console.log(myCar);
 //Оба свойства и public и без модификатора доступа доступны без ограничений.
 // Если мы применим private к свойству color, то TS сразу подсветит ошибку о том, что вне класса свойство не может применяться.
+//Добавив публичный метод changeColor, мы смогли изменить приватное свойство Color
